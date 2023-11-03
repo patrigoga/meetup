97b58cd244f38da03a0879de229afb5d1f2a7f8f
Patricio Gómez García <patrigoga@gmail.com>
2023-11-04 00:35:40 +0100

    iniciar servidor y routing

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+require('dotenv').config({path: 'variables.env'});
+
+
+const app = express();
+
+app.listen(process.env.PORT, () =>{
+    console.log('El servidor esta fucnionando');
+})
